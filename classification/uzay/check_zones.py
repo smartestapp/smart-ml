c7bec0b0cf94dea937ea079b6d9acab9254d0d2d
Uzay Macar <oum2000@columbia.edu>
2021-05-11 08:06:22 -0400

    Fixed line indentation problem in GitHub.

--- classification/uzay/check_zones.py
+++ classification/uzay/check_zones.py
@@ -13,7 +13,7 @@ args = parser.parse_args()
 
 output_dir = '%s-zones' % args.kit_id
 if not os.path.exists(output_dir):
-	os.makedirs(output_dir)
+    os.makedirs(output_dir)
 print('The cropped zones will be saved to %s' % output_dir)
 
 kit_data = json.load(open(args.json_path, 'r'))[args.kit_id]
@@ -23,10 +23,10 @@ membrane_paths = [os.path.join(args.membranes_dir, f) for f in os.listdir(args.m
                   if f.endswith('.jpg') or f.endswith('.JPG') or f.endswith('.JPEG') or f.endswith('.jpeg') or f.endswith('.png') or f.endswith('.PNG')]
 
 for p in tqdm(membrane_paths, desc='Cropping Zones & Saving Them'):
-	membrane = cv2.imread(p)
-	height, width, num_channels = membrane.shape
+    membrane = cv2.imread(p)
+    height, width, num_channels = membrane.shape
 
-	for i in range(num_zones):
-		coordinates = kit_data['dimensions']['zones']['zone%d' % (i + 1)]
-		zone = membrane[int(coordinates['y'] * height): int(coordinates['y'] * height) + int(coordinates['h'] * height), :, :]
-		cv2.imwrite(os.path.join(output_dir, '%s-zone%d.jpg' % (os.path.splitext(os.path.split(p)[-1])[0], i)), zone)
+    for i in range(num_zones):
+        coordinates = kit_data['dimensions']['zones']['zone%d' % (i + 1)]
+        zone = membrane[int(coordinates['y'] * height): int(coordinates['y'] * height) + int(coordinates['h'] * height), :, :]
+        cv2.imwrite(os.path.join(output_dir, '%s-zone%d.jpg' % (os.path.splitext(os.path.split(p)[-1])[0], i)), zone)
