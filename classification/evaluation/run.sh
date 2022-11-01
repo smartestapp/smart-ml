b34ad3bdc60b7dfc859fe8c9fffc74e04f0507c3
Uzay Macar <uzay.macar@gmail.com>
2022-11-01 13:59:20 +0000

    Deleted old files.

--- classification/evaluation/run.sh
+++
@@ -1,6 +0,0 @@
-# For testing/verification purpose, run the following command
-# a prediction.xlsx file will be generated
-python main.py --resolution low
-
-# An example of how to adjust code for testing different models, you can use 
-python main.py --resolution high --thredhold 0.0147
--- classification/full-training/run_self.sh
+++
@@ -1,9 +0,0 @@
-ARG_XONLY=0
-ARG_INIT=1
-
-set -x
-
-for ARG_PRELR in 0.0005
-do
-    CUDA_VISIBLE_DEVICES=3 python main.py --phase=self_train --setname=BTNx --lr=${ARG_PRELR} --xonly=${ARG_XONLY} --self_init=${ARG_INIT}
-done
